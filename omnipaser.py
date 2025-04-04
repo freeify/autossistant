f6425ed32e9a3800712f3cc94c247c5eac898c0f
emataranyika <emataranyika@eftcorp.com>
2025-04-04 09:06:53 +0200

    ClickAnyIcon

--- omnipaser.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoProcessor, AutoModelForVisualQuestionAnswering
-
-processor = AutoProcessor.from_pretrained("microsoft/OmniParser")
-model = AutoModelForVisualQuestionAnswering.from_pretrained("microsoft/OmniParser")
